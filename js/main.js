fed71f7f850ef826f065360f9d4af1ca9fa0992a
Perry <perryjhoffman@gmail.com>
2013-12-19 02:56:47 -0800

    Adding content, randomized header background and color, favicon.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+var imgs        = [{url: 'img/trees.jpg', color: '#62A894'}, {url: 'img/mountains.jpg', color: '#6289A8'}];
+var randomIndex = Math.floor(Math.random()*((imgs.length - 1)-0+1)+0);
+var header      = document.getElementsByClassName('header')[0];
+var logo        = document.getElementsByClassName('logo')[0];
+header.style.backgroundImage = "url(" + imgs[randomIndex].url + ")";
+logo.style.backgroundColor = imgs[randomIndex].color;
+
 $(document).ready(function(){
+  var $window = $(window);
+
   <!-- Scroll To -->
   $('.nav a').click(function(e){
     e.preventDefault();
@@ -7,4 +16,19 @@ $(document).ready(function(){
       scrollTop: $(selector).offset().top
     }, 1000);
   });
+
+  <!-- Fade Header -->
+  $('section.header .nav').fadeIn(2000);
+
+  <!-- Watch Scrollbar -->
+  $window.scroll(function(e){
+    var scrollHeight = $window.scrollTop();
+    var $btn         = $('.top-btn');
+    if (scrollHeight >= 300) {
+      $btn.fadeIn('slow');
+    } else if($btn.css('display') != 'none') {
+      $btn.fadeOut('slow');
+    }
+  });
 });
+$(document).foundation();
